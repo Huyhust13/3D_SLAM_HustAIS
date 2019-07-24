0206a005c5aa1f4cb964da8cb0bce7c5b104d767
huyhust13 <huyhust13@gmail.com>
2019-07-24 09:13:49 +0700

    asd

--- PSMNet/run_submission.sh
+++ PSMNet/run_submission.sh
@@ -1,5 +1,5 @@
-python submission.py --datapath ../../2.Data/KITTI/data_scene_flow/testing/ \
-                     --loadmodel ../../2.Data/KITTI/pretrained_model_KITTI2015.tar \
+python submission.py --datapath ../../data \
+                     --loadmodel ../pretrained_model_KITTI2015.tar \
                     #   --maxdisp 192 \
                     #  --model stackhourglass \
                     #  --KITTI 2015 \
